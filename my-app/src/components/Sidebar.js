3ac75e5bd64d66fcd8c458b81e4c1f38d8f8f607
Alisha <alishaanderssonn@icloud.com>
2025-03-11 19:26:08 +0100

    Link sidebar css to sidebar component file

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styles from "./css/Sidebar.module.css";
 
 function Sidebar() {
     return (
-        <div>
+        <div className={styles.sidebar}>
             <a href = "#." target= "_blank">My Photos</a>
             <a href = "#." target= "_blank">My Illustrations</a>
             <a href = "#." target= "_blank">My Paintings</a>
