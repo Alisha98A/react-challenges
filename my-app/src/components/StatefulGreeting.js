8318820fadf8635e3ff2a73babdf182979de1c75
Alisha <alishaanderssonn@icloud.com>
2025-03-13 17:18:01 +0100

    m

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -5,17 +5,23 @@ class StatefulGreeting extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            introduction: "Hello!!!!",
+            introduction: "Hello!",
             buttonText: "Exit",
         };
     }
 
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!"
+        });
+        console.log(this.state.introduction);
+    }
 
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
