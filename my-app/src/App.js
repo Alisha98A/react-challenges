55537ec69a1d574d0eb476a3ebed1505c7bc424c
Alisha <alishaanderssonn@icloud.com>
2025-03-17 18:28:33 +0100

    a

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,10 @@
-import css from "./App.module.css";
-// import NavBarSimple from "./components/NavBarSimple";
-import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm";
+import "./App.module.css";
+import RenderingLists from "./components/RenderingLists";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      <NavBarForm />
-      <Sidebar />
-    </div>
-  );
+  return <div className="App">
+      <RenderingLists />
+  </div>;
 }
 
 export default App;
