3fff7ef43ff186705a3ed1c91f3de9b46b18327a
Alisha <alishaanderssonn@icloud.com>
2025-03-23 10:32:21 +0100

    s

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function UseEffectCounter() {
   const [count, setCount] = useState(0);
+  const [time, setTime] = useState(0);
   useEffect(() => {
     console.log("count 1 effect");
     document.title = count;
@@ -11,6 +12,19 @@ function UseEffectCounter() {
     console.log("count 2 effect");
     document.title = count2;
   }, [count2]);
+
+  useEffect(() => {
+    console.log('Creating timer');
+    const interval = setInterval(() => {
+        console.log('Interval executed');
+        setTime(time => time + 1)
+    }, 1000);
+    return() => {
+        console.log('cleaning up')
+        clearInterval(interval);
+    }
+  }, []);
+
   return (
     <div>
       <button onClick={() => setCount((count) => count + 1)}>
@@ -19,6 +33,7 @@ function UseEffectCounter() {
       <button onClick={() => setCount2((count2) => count2 + 1)}>
         Increment Count2 ({count2})
       </button>
+      <h2>Time is {time}</h2>
     </div>
   );
 }
