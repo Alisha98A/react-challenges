49a08621729c27e5bdec2884d266cecbefdf256f
Alisha <alishaanderssonn@icloud.com>
2025-03-23 08:27:01 +0100

    s

--- my-app/src/components/UseStateWithArrays.js
+++ my-app/src/components/UseStateWithArrays.js
@@ -13,26 +13,26 @@ function UseStateWithArrays() {
         )
     }
     const buggyPushNums = () => {
-        nums.push(nums.lenght + 1);
+        nums.push(nums.length + 1);
         setNums(nums);
         console.log(nums);
     }
-  return (
-    <div>
-        <button onClick={addNums}>
-            Add Item
-        </button>
-        <button onClick={removeNum}>
-            Remove Item
-        </button>
-        <button onClick={buggyPushNums}>
-            Buggy Push Item
-        </button>
-        <ul>
-            {nums.map(num => <li key={num}>{num}</li>)}
-        </ul>
-    </div>
-  )
+    return (
+        <div>
+            <button onClick={addNums}>
+                Add Item
+            </button>
+            <button onClick={removeNum}>
+                Remove Item
+            </button>
+            <button onClick={buggyPushNums}>
+                Buggy Push Item
+            </button>
+            <ul>
+                {nums.map(num => <li key={num}>{num}</li>)}
+            </ul>
+        </div>
+    )
 }
 
 export default UseStateWithArrays
